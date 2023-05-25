fa3014fa48b0b2b5c07bcdb31067efa06714aec2
João Dias <joaoeduardodias123@gmail.com>
2023-05-25 10:45:10 -0300

    feat: create component Box

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,29 +1,3 @@
-import { styled } from './styles'
+// eslint-disable-next-line prettier/prettier
+export * from './components/Box';
 
-import { ComponentProps } from 'react'
-
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite500',
-  borderRadius: '$md',
-  border: 'none',
-  fontWeight: 'bold',
-  color: '$white',
-
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: '$2 $4',
-      },
-      big: {
-        fontSize: 14,
-        padding: '$3 $6',
-      },
-    },
-  },
-  defaultVariants: {
-    size: 'small',
-  },
-})
-export type ButtonProps = ComponentProps<typeof Button>
