cd33bd200162d9a7ba32a2717941c242b03c0ba5
João Dias <joaoeduardodias123@gmail.com>
2023-05-25 11:30:24 -0300

    feat: create component Avatar

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable prettier/prettier */
+export * from './components/Avatar';
 export * from './components/Box';
 export * from './components/Heading';
 export * from './components/Text';
