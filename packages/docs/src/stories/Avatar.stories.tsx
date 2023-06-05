ec9f8029872b357c1cec119c9560d196f3ed65bc
João Dias <joaoeduardodias123@gmail.com>
2023-06-05 10:20:28 -0300

    docs: add args type

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -9,6 +9,13 @@ export default {
     src: 'https://github.com/joaoeduardodias.png',
     alt: 'João Dias',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -8,6 +8,29 @@ export default {
   args: {
     children:
       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati perspiciatis perferendis autem repellat aliquam cumque distinctio sequi velit nesciunt dolores? Suscipit eaque recusandae sed libero accusamus corrupti illo mollitia id.',
+    size: 'md',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
